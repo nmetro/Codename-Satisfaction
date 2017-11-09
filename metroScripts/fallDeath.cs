60ddb28add80abbd10176aad8eb078632f9ac161
Nicholas Metro <31973556+nmetro@users.noreply.github.com>
2017-11-08 22:44:50 -0500

    11-8-17 Nick's Scripts

---
+++ metroScripts/fallDeath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallDeath : MonoBehaviour {
+
+	public float bottom;
+	public Transform respawnPoint;
+
+	//initialization
+	void Start () {
+		respawnPoint.position = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < bottom) {
+			respawn ();
+		}
+	}
+	public void respawn () {
+		transform.position = respawnPoint.position;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Enemy") {
+			respawn ();
+		}
+	}
+}
---
+++ metroScripts/movementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Simply moves the current game object
+public class movementScript : MonoBehaviour
+{
+
+	public float speed;
+	public GameObject target;
+
+	public Vector2 maxBounds;
+	public Vector2 minBounds;
+
+	public int tickLimit;
+	private int tick;
+
+	public Transform respawnPoint;
+
+	void Update()
+	{
+		Vector2 pos = GameObject.Find(target.name).transform.position;
+
+		float step = speed * Time.deltaTime;
+		if ((pos.x > minBounds.x && pos.x < maxBounds.x) && (pos.y > minBounds.y && pos.y < maxBounds.y)){
+			transform.position = Vector2.MoveTowards (transform.position, pos, step);
+			tick = 0;
+		} else
+			tick++;
+		
+		if (tick > tickLimit) {
+			transform.position = Vector2.MoveTowards (transform.position, respawnPoint.position, step);
+		}
+	}
+
+	public void respawn () {
+		transform.position = respawnPoint.position;
+	}
+
+}
